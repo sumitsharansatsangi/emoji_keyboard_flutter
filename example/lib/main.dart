197fbea999694256c24c569234d7d4aa0a0b8209
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-08-23 19:25:25 +0530

    dependencies updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool showEmojiKeyboard;
+  late bool showEmojiKeyboard;
   final TextEditingController controller = TextEditingController();
 
   @override
